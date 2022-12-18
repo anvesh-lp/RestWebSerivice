8b43d46b0f17d071dd46f5988931c46fcff84569
anvesh-lp <knanvesh.01@gmail.com>
2022-12-18 17:35:55 -0500

    Initial commit

---
+++ src/main/java/com/example/restwebserivice/RestWebSeriviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restwebserivice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebSeriviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWebSeriviceApplication.class, args);
+    }
+
+}
