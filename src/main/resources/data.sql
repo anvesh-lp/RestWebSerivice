bead7cddc9d23e63c7b6668f0516e8ee74446727
anvesh-lp <knanvesh.01@gmail.com>
2022-12-19 15:44:24 -0500

    Adding JPA functionality to posts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
-insert into User_details
+insert into User_details (id, birth_date, name)
 values (1001, current_date(), 'anvesh');
-insert into User_details
+insert into User_details (id, birth_date, name)
 values (1002, current_date(), 'Vamshi');
-insert into User_details
+insert into User_details (id, birth_date, name)
 values (1003, current_date(), 'Mani');
-insert into User_details
+insert into User_details (id, birth_date, name)
 values (1004, current_date(), 'Kavya');
-insert into User_details
+insert into User_details (id, birth_date, name)
 values (1005, current_date(), 'Ganesh');
-insert into User_details
+insert into User_details (id, birth_date, name)
 values (1006, current_date(), 'Nandhan');
 
 insert into post (id, description, user_id)
